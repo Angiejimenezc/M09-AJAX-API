79a9cac1d9bd02d06385449aa36c986f36aa913e
Angie Jimenez <angiejimenez@gmail.com>
2021-06-17 09:49:07 +0200

    first commit Nivel 1-2

---
+++ Nivel1-2/main.js
@@ -0,0 +1,22 @@
+jokes(); //Primera vez que se carga aplicación muestra primer chiste
+
+document.querySelector("#btnGetJoke").addEventListener("click", jokes);
+
+function jokes() {
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+
+  xhttp.send();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let datos = JSON.parse(this.responseText);
+      console.log(datos);
+      let joke = document.querySelector("#quote");
+
+      console.log(datos.value.joke);
+      joke.innerHTML = datos.value.joke;
+    }
+  };
+}
